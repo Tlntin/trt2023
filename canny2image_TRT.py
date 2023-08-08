10901b9c791bbb40686ec404778cdd2abf7bcd13
Tlntin <TlntinDeng01@Gmail.com>
2023-08-08 01:34:58 +0000

    set step=8

--- canny2image_TRT.py
+++ canny2image_TRT.py
@@ -677,7 +677,7 @@ class hackathon():
             samples = self.ddim_sampler.sample(
                 control=control,
                 batch_crossattn=batch_crossattn,
-                ddim_num_steps=7,
+                ddim_num_steps=8,
                 eta=eta,
                 uncond_scale=scale,
                 batch_size=num_samples,
